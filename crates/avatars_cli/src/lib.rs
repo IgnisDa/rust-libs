acf5fece5c2ebe6c92f24d789c28dcf1ce3aa5f1
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-12-29 19:44:14 +0530

    feat(avatars_cli): add code to generate avatars

--- crates/avatars_cli/src/lib.rs
+++ crates/avatars_cli/src/lib.rs
@@ -5,7 +5,16 @@ use cli::{Gender, Mood};
 use color_eyre::Result;
 
 pub fn run_command(seed_string: String, mood: Mood, gender: Gender) -> Result<String> {
-    Ok("".to_string())
+    let seed = generate_seed(&seed_string);
+    let mood = match mood {
+        Mood::Happy => AvatarMood::Happy,
+        Mood::Sad => AvatarMood::Sad,
+        Mood::Surprised => AvatarMood::Surprised,
+    };
+    Ok(match gender {
+        Gender::Female => female_avatar(seed, mood),
+        Gender::Male => male_avatar(seed, mood),
+    })
 }
 
 pub use cli::Cli;
