5aedfbe435701857b39dd2f0ae1d9b6fdd704695
Diptesh <ignisda2001@gmail.com>
2023-01-11 03:06:06 +0000

    ci: remove helix specific scripts

--- .helix/init-helix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eux
-
-# Copy generated keys
-mkdir -p $HOME/.ssh
-cat /.helix/tmp/temp-ssh-key.pub > $HOME/.ssh/authorized_keys
-chmod 644 $HOME/.ssh/authorized_keys
-chmod 700 $HOME/.ssh
